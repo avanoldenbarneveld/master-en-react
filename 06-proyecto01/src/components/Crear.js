406f19f5302075e713d991144d72e225368fd097
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-09-29 16:25:01 +0200

    use localStorage to save movie array object

--- 06-proyecto01/src/components/Crear.js
+++ 06-proyecto01/src/components/Crear.js
@@ -27,8 +27,38 @@ export const Crear = () => {
             titulo,
             descripcion
          }
-
+        // Guardar estado
          setPeliState(peli)
+
+        // Guardar en el almacenamiento local
+        guardarEnStorage(peli)
+
+
+    }
+
+    const guardarEnStorage = peli => {
+
+        // Conseguir los elementos que ya tenemos en el Localstorage
+        let elementos = JSON.parse(localStorage.getItem("pelis"));
+
+        // Comprobar si es un array
+
+        if(Array.isArray(elementos)){
+            // Añadir un elemento nuevo
+            elementos.push(peli)
+        } else {
+            // Crear un array con la nueva peli
+            elementos = [peli];
+        }
+
+        // Guardar en el localstorage
+        localStorage.setItem("pelis", JSON.stringify(elementos));       
+
+        // Devolver objeto guardado
+        return peli;
+
+        
+
     }
 
   return (
