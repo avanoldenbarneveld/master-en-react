e468ff08fb009d2a3c9e0a744158b5a20c481d21
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-09-30 08:31:33 +0200

    replace hardcoded articles with dynamic rendering using useState, useEffect and localStorage

--- 06-proyecto01/src/components/Listado.js
+++ 06-proyecto01/src/components/Listado.js
@@ -1,39 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Listado = () => {
+
+  const [listadoState, setListadoState] = useState([])
+
+  useEffect(() => {
+  console.log("Componentes del listado de peliculas cargado")
+  conseguirPeliculas();
+  }, []);
+
+  const conseguirPeliculas = () => {
+    let peliculas = JSON.parse(localStorage.getItem("pelis"));
+
+    setListadoState(peliculas);
+  }
+
   return (
     <>
-            <article className="peli-item">
-                <h3 className="title">Desarollo web</h3>
-                <p className="description">victorroblesweb.es</p>
+      { listadoState != null ? 
+              listadoState.map(peli => {
+        return (
+              <article key={peli.id} className="peli-item">
+                <h3 className="title">D{peli.titulo}</h3>
+                <p className="description">{peli.descripcion}</p>
 
                 <button className="edit">Editar</button>
                 <button className="delete">Borrar</button>
             </article>
+        )
+      })
+      :  <h2> No hay películas para mostrar </h2>
+    }
 
-            <article className="peli-item">
-                <h3 className="title">Desarollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-
-            <article className="peli-item">
-                <h3 className="title">Desarollo web</h3>
-                <p className="description">victorroblesweb.es</p>
-
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
     </>
   )
 }
