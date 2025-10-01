0a5baa1676c02cba951dfbdf7c2c40d5c15e1737
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-01 08:08:02 +0200

    feat: ability to delete movies

--- 06-proyecto01/src/components/Listado.js
+++ 06-proyecto01/src/components/Listado.js
@@ -2,10 +2,32 @@ import React, { useEffect } from 'react';
 
 export const Listado = ({ listadoState, setListadoState }) => {
   useEffect(() => {
-    console.log("Componentes del listado de peliculas cargado");
+    conseguirPeliculas();
+  }, []);
+
+  const conseguirPeliculas = () => {
     let peliculas = JSON.parse(localStorage.getItem("pelis")) || [];
     setListadoState(peliculas);
-  }, [setListadoState]);
+    return peliculas;
+  };
+
+  const borrarPeli = (id) => {
+    // Conseguir películas almacenadas
+    let pelis_almacenadas = conseguirPeliculas();
+
+    // Filtrar las películas para eliminar la que no quiero
+    let nuevo_array_pelis = pelis_almacenadas.filter(
+      peli => peli.id !== parseInt(id)
+    );
+
+    console.log(pelis_almacenadas, nuevo_array_pelis)
+
+    // Actualizar estado del listado
+    setListadoState(nuevo_array_pelis);
+
+    // Actualizar los datos en localStorage
+    localStorage.setItem("pelis", JSON.stringify(nuevo_array_pelis));
+  };
 
   return (
     <>
@@ -15,7 +37,12 @@ export const Listado = ({ listadoState, setListadoState }) => {
             <h3 className="title">{peli.titulo}</h3>
             <p className="description">{peli.descripcion}</p>
             <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
+            <button
+              className="delete"
+              onClick={() => borrarPeli(peli.id)}
+            >
+              Borrar
+            </button>
           </article>
         ))
         : <h2>No hay películas para mostrar</h2>
