9855201409425bfcea28a972ff493fbf922c819e
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-01 12:39:40 +0200

    add search functionality with Buscador component

--- 06-proyecto01/src/components/Buscador.js
+++ 06-proyecto01/src/components/Buscador.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const Buscador = () => {
+export const Buscador = ({listadoState, setListadoState}) => {
+
+  const [busqueda, setBusqueda] = useState('');
+
+  const buscarPeli = (e) => {
+
+    // Crear estado y actualizarlo
+    setBusqueda(e.target.value)
+    // Filtrar para buscar coincidencias
+    let pelis_encontradas = listadoState.filter(peli => {
+      return peli.titulo.toLowerCase().includes(busqueda.toLocaleLowerCase())
+    });
+    // Comprobar si hay un resultado
+    if(busqueda.length <= 1) {
+      pelis_encontradas = JSON.parse(localStorage.getItem("pelis"));
+    }
+    // Actualizar estado del listado principal con lo que he filtrado
+    setListadoState(pelis_encontradas)
+  }
+
   return (
                 <div className="search">
-                    <h3 className="title">Buscador</h3>
+                    <h3 className="title">Buscador: {busqueda}</h3>
                     <form>
-                        <input type="text" />
+                        <input type="text"
+                          id="search_field"
+                          name="busqueda"
+                          autoComplete='off'
+                          onChange={buscarPeli}
+                        />
                         <button>Buscar</button>
                     </form>
             </div>
