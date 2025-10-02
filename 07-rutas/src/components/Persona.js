8bac09385a95500e67e0cc4f8f908587e2915981
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-02 16:05:28 +0200

    update persona logic

--- 07-rutas/src/components/Persona.js
+++ 07-rutas/src/components/Persona.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const Persona = () => {
 
@@ -7,12 +7,39 @@ export const Persona = () => {
     //let {nombre="Víctor", apellido="Robles"} = useParams();
 
     const {nombre, apellido} = useParams();
+    const navegar = useNavigate();
 
+    const enviar = (e) => {
+      e.preventDefault();
+      let nombre = e.target.nombre.value;
+      let apellido = e.target.apellido.value;
+      let url = `/persona/${nombre}/${apellido}`;
+
+      if(nombre.length <= 0 && apellido.length <= 0){
+        navegar("/inicio")
+
+        } else if (nombre === "contacto"){
+            navegar("/contacto");
+
+        } else {
+          navegar(url);
+        }
+  };
+    
   return (
     <div>
         {!nombre && <h1>No hay ninguna persona que mostrar</h1>}
         {nombre && <h1>Página de persona: {nombre} {apellido}</h1>}
+
         <p>Esta es la página de persona</p>
+
+        <form onSubmit={enviar}>
+          <input type='text' name='nombre' />
+          <input type='text' name='apellido' />
+          <input type='submit' name='enviar' value="enviar" />
+        </form>
+
     </div>
   )
 }
+
