dff1814327be0c963cfe818c3cdbb14458a354d8
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-02 09:20:29 +0200

    setup initial routing for main pages

---
+++ 07-rutas/src/components/Contacto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Contacto = () => {
+  return (
+    <div>
+        <h1> Página de inicio</h1>
+        <p> Esta es la página de inicio</p>
+    </div>
+  )
+}
