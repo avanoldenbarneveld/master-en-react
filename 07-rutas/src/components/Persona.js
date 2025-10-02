9b2a265ddadf275fa111efc5bd54c792fc8eefa5
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-02 10:12:25 +0200

    add name and last names to url and print with useParams

---
+++ 07-rutas/src/components/Persona.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+export const Persona = () => {
+
+    const {nombre, apellido} = useParams();
+
+  return (
+    <div>
+        <h1>Página de persona: {nombre} {apellido}</h1>
+        <p>Esta es la página de persona</p>
+    </div>
+  )
+}
