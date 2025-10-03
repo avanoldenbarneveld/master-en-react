ed52a986012241e170a28061071e9219edf6e986
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-03 09:15:17 +0200

    setup layout and create HeaderNav and Footer components

---
+++ 08-proyecto02/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer>
+        Portafolio ALberto van Oldenbarneveld WEB &copy; Máster en React - 2025
+    </footer>
+  )
+}
--- 08-proyecto02/src/router/MisRutas.js
+++ 08-proyecto02/src/router/MisRutas.js
@@ -5,12 +5,14 @@ import { Portafolio } from '../components/Portafolio';
 import { Servicios } from '../components/Servicios';
 import { Curriculum } from '../components/Curriculum';
 import { Contacto } from '../components/Contacto';
+import { HeaderNav } from '../components/layout/HeaderNav';
+import { Footer } from '../components/layout/Footer';
 
 export const MisRutas = () => {
   return (
     <BrowserRouter>
         {/* HEADER Y NAVEGACION */}
-
+        <HeaderNav />
         {/* CONTENIDO CENTRAL */}
 
         <Routes>
@@ -21,6 +23,9 @@ export const MisRutas = () => {
             <Route path='/curriculum' element={<Curriculum/>} />
             <Route path='/contacto' element={<Contacto/>} />
         </Routes>
+
+        {/* Footer */}
+        <Footer />
     </BrowserRouter>
   )
 }
