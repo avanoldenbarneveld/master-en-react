b39e4d259763f1c01b43e8adfce9531766bd56b7
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-03 13:44:50 +0200

    add additional styling and structuring to page

--- 08-proyecto02/src/components/Inicio.js
+++ 08-proyecto02/src/components/Inicio.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div>
+
+        <h1>
+          Hola, soy Alberto van Oldenbarneveld y soy Desarrollador Web en Pamplona,
+          y ofrezco mis servicios de programación y desarrollo en
+          todo tipo de proyectos web.
+        </h1>
+
+        <h2>
+          Te ayudo a crear tu sitio o aplicación web, tener más
+          visibilidad y relevancia en internet. <Link to='/contacto'> Contacta conmigo.</Link>
+        </h2>
+
+        <section className='latest-projects'>
+          <h2>Algunos de mis proyectos</h2>
+          <p> Estos son algunos de mis trabajos de desarrollo web.</p>
+
+          <div className='projects'>
+
+          </div>
+        </section>
+    </div>
+  
   )
 }
--- 08-proyecto02/src/components/Servicios.js
+++ 08-proyecto02/src/components/Servicios.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export const Servicios = () => {
   return (
-    <div>Servicios</div>
+    <div className='page'>
+      <h1>Servicios</h1>
+
+        <section className='services'>
+          <article className='service'>
+              <h2>Diseño web</h2>
+              <p>Hago que tu web se vea bonita</p>
+          </article>
+          <article className='service'>
+              <h2>Desarollo web</h2>
+              <p>Creo tu página web desde cero</p>
+          </article>
+          <article className='service'>
+              <h2>Diseño web</h2>
+              <p>Hago que tu web aparezca en Google y la vea la gente</p>
+          </article>
+
+        </section>
+      </div>
   )
 }
