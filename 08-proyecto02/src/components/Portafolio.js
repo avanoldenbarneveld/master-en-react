e48526ebc2163b24852bbd6c0fe964467cf3b026
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-06 08:40:43 +0200

    setup mock data to be imported on portafolio page

--- 08-proyecto02/src/components/Portafolio.js
+++ 08-proyecto02/src/components/Portafolio.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { trabajos  } from '../data/trabajos'
 
 export const Portafolio = () => {
   return (
-    <div className='page'>Portafolio</div>
+    <div className='page'>
+      <h1 className='heading'>Portafolio </h1>
+
+      {
+        trabajos.map(trabajo => {
+          <article>
+            <h2>{trabajo.nombre}</h2>
+          </article>
+        })
+      }
+
+    </div>
   )
 }
---
+++ 08-proyecto02/src/data/trabajos.js
@@ -0,0 +1,58 @@
+export const trabajos = [
+  {
+    id: 'albertovan.com',
+    nombre: 'Alberto van Oldenbarneveld',
+    url: 'https://albertovan.com',
+    tecnologias: 'HTML, CSS, JS, ASTRO',
+    categorias: 'Desarrollo web'
+  },
+  {
+    id: 'inbiot-fullstack-test',
+    nombre: 'Inbiot Full Stack Test',
+    url: 'https://inbiot-fullstack-test.vercel.app',
+    tecnologias: 'React, Node.js, Express, MongoDB, Vercel, Render',
+    categorias: 'Full Stack'
+  },
+  {
+    id: 'popular-movies-app',
+    nombre: 'Popular Movies App',
+    url: 'https://github.com/avanoldenbarneveld/popular-movies-app',
+    tecnologias: 'React, React Router, API REST, Testing Library',
+    categorias: 'Frontend'
+  },
+  {
+    id: 'auth-system-react',
+    nombre: 'Sistema de Autenticación en React',
+    url: 'https://github.com/avanoldenbarneveld/bootcamp-projects/tree/main/8.%20React',
+    tecnologias: 'React, JWT, Node.js, Express, Protected Routes',
+    categorias: 'Full Stack'
+  },
+  {
+    id: 'express-user-api',
+    nombre: 'API de Gestión de Usuarios',
+    url: 'https://github.com/avanoldenbarneveld/bootcamp-projects/tree/main/7.%20Express',
+    tecnologias: 'Express, Node.js, Docker, JWT, REST API',
+    categorias: 'Backend'
+  },
+  {
+    id: 'task-manager-node',
+    nombre: 'Task Manager App',
+    url: 'https://github.com/avanoldenbarneveld/bootcamp-projects/tree/main/5.%20Node.js',
+    tecnologias: 'Node.js (sin frameworks), File System, HTTP module',
+    categorias: 'Backend'
+  },
+  {
+    id: 'sql-databases',
+    nombre: 'SQL & PostgreSQL',
+    url: 'https://github.com/avanoldenbarneveld/bootcamp-projects/tree/main/6.%20Databases',
+    tecnologias: 'PostgreSQL, SQL, Joins, Data Modeling',
+    categorias: 'Data'
+  },
+  {
+    id: 'networking-nginx',
+    nombre: 'Networking Project with Nginx',
+    url: 'https://github.com/avanoldenbarneveld/bootcamp-projects/blob/main/1.%20Networking%20project/readme.MD',
+    tecnologias: 'Nginx, Static Hosting, Error Handling, Authentication',
+    categorias: 'Infraestructura'
+  },
+]
