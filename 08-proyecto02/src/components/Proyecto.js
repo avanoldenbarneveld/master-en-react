cc445b841bbf22aa549cb99fafe3aaa629f803c5
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-06 10:12:18 +0200

    add dynamic project detail page with useParams and state management

---
+++ 08-proyecto02/src/components/Proyecto.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { trabajos } from '../data/trabajos';
+
+export const Proyecto = () => {
+
+    const [proyecto, setProyecto] = useState({});
+    const params = useParams();
+
+    useEffect(()=>{
+        let proyecto = trabajos.filter(trabajo => trabajo.id === params.id);
+        setProyecto(proyecto[0])
+    }, []);
+
+  return (
+    <div className='page page-project'>
+        <div className='mask'>
+            <img src={"/images/"+proyecto.id+".jpg"} alt='' />
+        </div>
+    <h1 className='heading'>{proyecto.nombre}</h1>
+    <p>{proyecto.tecnologias}</p>
+    <p>{proyecto.descripcion}</p>
+    <a href={"https://"+proyecto.url} target='_blank' rel='noreferrer'> Ir al proyecto</a>
+    </div>
+  )
+}
