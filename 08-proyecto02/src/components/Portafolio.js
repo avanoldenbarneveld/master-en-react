3363cc98b38e2957cdde2dbd0b0a5f6b3c73b08a
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-06 09:31:41 +0200

    add images for link to projects plus styling

--- 08-proyecto02/src/components/Portafolio.js
+++ 08-proyecto02/src/components/Portafolio.js
@@ -7,14 +7,21 @@ export const Portafolio = () => {
     <div className='page'>
       <h1 className='heading'>Portafolio</h1>
 
+      <section className='projects'>
       {
       trabajos.map(trabajo => (
-        <article key={trabajo.id}>
+        <article key={trabajo.id} className='project-item'>
+          <div className='Mask'>
+             <img src={"/images/"+trabajo.id+".jpg"} />
+          </div>
           <span>{trabajo.categorias}</span>
           <h2><Link to={"/proyecto/"+trabajo.id}>{trabajo.nombre}</Link></h2>
           <h3>{trabajo.tecnologias}</h3>
         </article>
       ))}
+      
+      </section>
+
     </div>
   )
 }
