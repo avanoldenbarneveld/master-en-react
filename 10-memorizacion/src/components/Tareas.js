144fddf3ea45098a5262a8d7938a25a08b87c842
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-07 09:49:36 +0200

    add task form with state and dynamic task list rendering

--- 10-memorizacion/src/components/Tareas.js
+++ 10-memorizacion/src/components/Tareas.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Tareas = () => {
+    const [tareas, setTareas] = useState([]);
+
+    const guardarTareas = e => {
+        e.preventDefault();
+
+        let tareas_actualizadas = [...tareas, e.target.descripcion.value]
+
+        setTareas(tareas_actualizadas);
+
+        console.log(tareas)
+    }
   return (
     <div className='tareas-container'>
         <h1>Mis tareas</h1>
-        <form>
+        <form onSubmit={guardarTareas}>
             <input type="text" name="descripcion" placeholder="Describe la tarea"/>
             <input type="submit" value="Guardar" />
         </form>
@@ -12,7 +23,13 @@ export const Tareas = () => {
         <hr/>
 
         <h3>Lista de tareas</h3>
-
+        <ul>
+            {
+                tareas.map((tarea, indice) => {
+                    return <li key={indice}>{tarea}</li>
+                })
+            }
+        </ul>
     </div>
   )
 }
