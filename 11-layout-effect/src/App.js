ba21ec9c31686018ec401a64a33f3c9b6e2180ec
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-08 10:51:50 +0200

    add refs and toggle logic to useEffect/useLayoutEffect demo

--- 11-layout-effect/src/App.js
+++ 11-layout-effect/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { EjemploComponent } from './components/EjemploComponent';
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <EjemploComponent />
       </header>
     </div>
--- 11-layout-effect/src/components/EjemploComponent.js
+++ 11-layout-effect/src/components/EjemploComponent.js
@@ -1,31 +1,39 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 export const EjemploComponent = () => {
 
-    const [mostrar, setMostrar] = useState(false)
+    const [mostrar, setMostrar] = useState(false);
+    
+    const caja = useRef();
+    const boton = useRef();
 
     useLayoutEffect(()=>{
         console.log("useLayoutEffect: Componente cargado !!!");
-        let caja = document.querySelector("#caja");
-        caja.innerHTML = "Hola";
-        console.log(caja.getBoundingClientRect());
     }, [])
 
     useEffect(()=>{
         console.log("useEffect: Componente cargado !!!");
-        let caja = document.querySelector("#caja");
-        caja.innerHTML = "Hola 2";
-        console.log(caja.getBoundingClientRect());
-    }, [])
+        if(caja.current == null) return
+
+        const {bottom} = boton.current.getBoundingClientRect();
+
+        console.log(bottom);
+
+        caja.current.style.top = `${bottom + 45}px`;
+        caja.current.style.left = `${bottom + 45}px`;
+
+    }, [mostrar])
 
   return (
     <div>
         <h1>Ejemplo useEffect y useLayoutEffect</h1>
-
-
+        
+        <button ref={boton} onClick={() => setMostrar(prev => {
+                console.log(!prev);
+            })}>Mostrar mensaje</button>
             {mostrar && (
-            <div id='caja'> 
-                Hola, soy un mensaje
+            <div id='caja' ref={caja} > 
+                Hola, soy un mensaje {mostrar}
             </div>
             )}
     </div>
