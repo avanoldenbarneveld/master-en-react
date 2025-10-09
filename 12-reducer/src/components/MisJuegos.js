d5c607c3eca5676ad9442ddaa19f443664e96127
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-09 11:04:47 +0200

    add form handler to capture form data

--- 12-reducer/src/components/MisJuegos.js
+++ 12-reducer/src/components/MisJuegos.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
 export const MisJuegos = () => {
+
+    const conseguirDatosForm = e => {
+        e.preventDefault();
+
+        let juego = {
+            id: new Date().getTime(),
+            titulo: e.target.titulo.value,
+            descripcion: e.target.descripcion.value
+        };
+
+
+        console.log(juego)
+    }
+
   return (
     <div>
         <h1>Estos son mis videojuegos</h1>
@@ -15,7 +29,7 @@ export const MisJuegos = () => {
 
         <h3>Agregar Juego</h3>
 
-        <form>
+        <form onSubmit={conseguirDatosForm}>
             <input type="text" name='titulo' placeholder='Titulo' />
             <textarea name='descripcion' placeholder='Descripción'></textarea>
             <input type='submit' value='Guardar' />
