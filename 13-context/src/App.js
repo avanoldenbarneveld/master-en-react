a0d25b49684cb5bd7c128c0a6115a9bd72fdf355
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-09 13:49:34 +0200

    initial project setup to learn hook useContext

---
+++ 13-context/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Aprendiendo el useContext</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
