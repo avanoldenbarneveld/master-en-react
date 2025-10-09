2f01e66918354cb5c5bdb2667553dd6475cdf2bf
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-09 10:14:47 +0200

    setup html and css, add MisJuegos component

---
+++ 12-reducer/src/components/MisJuegos.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const MisJuegos = () => {
+  return (
+    <div>
+        <h1>Estos son mis videojuegos</h1>
+
+        <p> Número de videojuegos: 15</p>
+        <ul>
+            <li>GTA</li>
+            <li>Mortal Kombat</li>
+            <li>Crash Bandicoot</li>
+        </ul>
+
+
+        <h3>Agregar Juego</h3>
+
+        <form>
+            <input type="text" name='titulo' placeholder='Titulo' />
+            <textarea name='descripcion' placeholder='Descripción'></textarea>
+            <input type='submit' value='Guardar' />
+        </form>
+    </div>
+  )
+}
