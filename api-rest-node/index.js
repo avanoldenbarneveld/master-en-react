3f0d63bc5b433233d4f339821e9a236c7645d065
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-22 09:30:17 +0200

    setup Express server with MongoDB connection, CORS, and JSON body parsing

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -1,4 +1,6 @@
 const { conexion } = require('./basedatos/conexion');
+const express = require('express')
+const cors =require('cors')
 
 // Inicializar app
 console.log('App de node arrancada')
@@ -6,3 +8,19 @@ console.log('App de node arrancada')
 // Conectar a la base de datos
 conexion()
 
+// Crear servidor Node
+const app = express();
+const puerto = 3900
+
+// Configurar cors
+app.use(cors());
+
+// Convertir body a objeto js
+app.use(express.json());
+
+// RUTAS
+
+// Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log('Servidor corriendo en el puerto '+puerto)
+})
