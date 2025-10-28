971c07ecc45daa1bf17a95f53293aaca85bbf56d
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-28 10:38:16 +0100

    update site title, create homepage component with styled jumbo section, and add navigation link to articles

--- 15-proyecto03/src/components/pages/Inicio.jsx
+++ 15-proyecto03/src/components/pages/Inicio.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import {Link } from 'react-router-dom'
 
 export const Inicio = () => {
   return (
-    <div>Inicio</div>
+    <div className='jumbo'> Inicio
+      <h1> Bienvenido al blog con React</h1>
+      <p> Blog desarollado con el MERN Stack (Mongo, Express, React y NodeJS)</p>
+      <Link to='/articulos' className='button'>Ver los articulos </Link>
+    </div>
   )
 }
