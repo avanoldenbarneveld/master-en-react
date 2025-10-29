073ec66f1f9bc8cdf1074c4afdc0d7391642b3dc
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-29 09:20:25 +0100

    fetch articles from API and render them conditionally

--- 15-proyecto03/src/components/pages/Articulos.jsx
+++ 15-proyecto03/src/components/pages/Articulos.jsx
@@ -6,45 +6,49 @@ export const Articulos = () => {
   const [articulos, setArticulos] = useState([])
 
   useEffect(() => {
-    let data = [
-      {
-        _id: 1,
-        titulo: 'Titulo 1',
-        contenido: 'Contenido'
-      },
-      {
-        _id: 2,
-        titulo: 'Titulo 1',
-        contenido: 'Contenido'
-      },
-      {
-        _id: 3,
-        titulo: 'Titulo 1',
-        contenido: 'Contenido'
-      },
-    ];
-
-    setArticulos(data);
+    conseguirArticulos();
   }, [])
+
+  const conseguirArticulos = async () => {
+    const url = 'http://localhost:3900/api/articulos';
+    let peticion = await fetch(url, {
+      method: 'GET'
+    });
+    let datos = await peticion.json();
+
+    if (datos.status === 'success') {
+      setArticulos(datos.articulos)
+    }
+  }
 
   return (
     <>
-      {articulos.map(articulo => {
-        return (
-        <article key= {articulo._id} className="articulo-item">
-          <div className='mascara'>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/330px-Unofficial_JavaScript_logo_2.svg.png' />
-          </div>
-          <div className='datos'>
-            <h3 className="title">{articulo.titulo}</h3>
-            <p className="description">{articulo.contenido}</p>
+      {
+        articulos.length >= 1 ?
+          (
+            articulos.map(articulo => {
+              return (
+                <article key={articulo._id} className="articulo-item">
+                  <div className='mascara'>
+                    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/330px-Unofficial_JavaScript_logo_2.svg.png' />
+                  </div>
+                  <div className='datos'>
+                    <h3 className="title">{articulo.titulo}</h3>
+                    <p className="description">{articulo.contenido}</p>
 
-            <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
-          </div>
-        </article>
-        );
-      })}
+                    <button className="edit">Editar</button>
+                    <button className="delete">Borrar</button>
+                  </div>
+                </article>
+              );
+            })
+
+      )
+      :
+      (
+      <h1> No hay artículos</h1>
+      )
+}
     </>
   )
 }
