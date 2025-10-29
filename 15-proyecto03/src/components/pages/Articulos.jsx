48c2e10f51e7e517db2fdb8f77bbf20a85651ac6
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-10-29 08:39:32 +0100

    add state management and mapping logic to render article list

--- 15-proyecto03/src/components/pages/Articulos.jsx
+++ 15-proyecto03/src/components/pages/Articulos.jsx
@@ -1,22 +1,50 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export const Articulos = () => {
+
+  const [articulos, setArticulos] = useState([])
+
+  useEffect(() => {
+    let data = [
+      {
+        _id: 1,
+        titulo: 'Titulo 1',
+        contenido: 'Contenido'
+      },
+      {
+        _id: 2,
+        titulo: 'Titulo 1',
+        contenido: 'Contenido'
+      },
+      {
+        _id: 3,
+        titulo: 'Titulo 1',
+        contenido: 'Contenido'
+      },
+    ];
+
+    setArticulos(data);
+  }, [])
+
   return (
     <>
-      <article className="articulo-item">
-        <div className='mascara'>
-          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/330px-Unofficial_JavaScript_logo_2.svg.png' />
-        </div>
+      {articulos.map(articulo => {
+        return (
+        <article key= {articulo._id} className="articulo-item">
+          <div className='mascara'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/330px-Unofficial_JavaScript_logo_2.svg.png' />
+          </div>
+          <div className='datos'>
+            <h3 className="title">{articulo.titulo}</h3>
+            <p className="description">{articulo.contenido}</p>
 
-        <div className='datos'>
-          <h3 className="title">Desarollo web</h3>
-          <p className="description">victorroblesweb.es</p>
-
-          <button className="edit">Editar</button>
-          <button className="delete">Borrar</button>
-        </div>
-      </article>
-
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+          </div>
+        </article>
+        );
+      })}
     </>
   )
 }
